158826524080c8c4f9b678e8aac92dd8118accc8
yzzzzun <yzzzzun@gmail.com>
2023-02-13 20:14:19 +0900

    feat: load balancer

--- first-service/src/main/java/com/yzzzzun/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/com/yzzzzun/firstservice/controller/FirstServiceController.java
@@ -1,13 +1,22 @@
 package com.yzzzzun.firstservice.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
+@RequiredArgsConstructor
 public class FirstServiceController {
+
+	private final Environment env;
 
 	@GetMapping("/welcome")
 	public String welcome(){
@@ -20,7 +29,11 @@ public class FirstServiceController {
 	}
 
 	@GetMapping("/check")
-	public String check(){
-		return "Hi, first service";
+	public String check(HttpServletRequest request){
+
+		log.info("Server port={}", request.getServerPort());
+
+		return String.format("Hi, first service PORT %s", env.getProperty("local.server.port"));
+
 	}
 }
