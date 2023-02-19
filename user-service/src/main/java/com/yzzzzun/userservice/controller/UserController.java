13bfc16dd004b14098e161f3eabc30774bc82854
yzzzzun <yzzzzun@gmail.com>
2023-02-19 18:41:10 +0900

    feat: api gateway routing 추가

--- user-service/src/main/java/com/yzzzzun/userservice/controller/UserController.java
+++ user-service/src/main/java/com/yzzzzun/userservice/controller/UserController.java
@@ -2,11 +2,13 @@ package com.yzzzzun.userservice.controller;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yzzzzun.userservice.domain.dto.UserDto;
@@ -19,16 +21,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController("/")
+@RestController
+@RequestMapping("/user-service")
 @RequiredArgsConstructor
 public class UserController {
 	private final Greeting greeting;
+
+	private final Environment env;
 
 	private final UserService userService;
 
 	@GetMapping("/health_check")
 	public String status() {
-		return "It's Working in User Service.";
+		return String.format("It's Working in User Service on PORT %s", env.getProperty("local.server.port"));
 	}
 
 	@GetMapping("/welcome")
@@ -45,5 +50,15 @@ public class UserController {
 		ResponseUser responseUser = mapper.map(userService.createUser(userDto), ResponseUser.class);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+	}
+
+	@GetMapping("/users")
+	public void getUsers() {
+
+	}
+
+	@GetMapping("/users/{userID}")
+	public void getUser() {
+
 	}
 }
